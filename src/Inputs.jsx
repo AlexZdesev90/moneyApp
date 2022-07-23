d13e96ac8cf1654c91a85f56bd33122f6c375a7f
Alexandr <zdesev-aleksandr@mail.ru>
2022-07-23 22:36:11 +0400

    delete unnecessary files

--- src/Inputs.jsx
+++ src/Inputs.jsx
@@ -1,6 +1,29 @@
 import React, { useState, useContext } from "react";
 import { Context } from './Context';
+import styled from 'styled-components';
 
+const Button = styled.a`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 9rem;
+  background: blue;
+  color: black;
+  border: 2px solid white;
+  @media (min-width: 768px) { 
+    padding: 1rem 2rem;
+    width: 11rem;
+  }
+  
+  @media (min-width: 1024px) { 
+    padding: 1.5rem 2.5rem;
+    width: 13rem;
+  }
+  :hover {
+    color: red;
+  }
+`
 
 const Inputs = () => {
   const [item, setItem] = useState("Phone");
@@ -29,7 +52,7 @@ const Inputs = () => {
         onChange={(e) => setCost(e.target.value)}
         
       />
-      <button
+      <Button danger
         onClick={() => {
           updateObject({
             title: item,
@@ -40,7 +63,7 @@ const Inputs = () => {
         }}
       >
         add
-      </button>
+      </Button>
     </div>
   );
 };
